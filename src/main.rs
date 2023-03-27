6fa4cab1aadc7c4c5b435a026616557d2a75178c
Jimmy Hartzell <jah259@cornell.edu>
2023-03-27 01:51:48 -0400

    add stupid sample grammar from lalrpop docs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod ast;
 
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar); // synthesized by LALRPOP
+
+#[test]
+fn grammar() {
+    assert_eq!(grammar::TermParser::new().parse("22"), Ok(22));
+    assert_eq!(grammar::TermParser::new().parse("(22)"), Ok(22));
+    assert_eq!(grammar::TermParser::new().parse("((((22))))"), Ok(22));
+    assert!(grammar::TermParser::new().parse("((22)").is_err());
+}
+
 fn main() {
     println!("Hello, world!");
 }
